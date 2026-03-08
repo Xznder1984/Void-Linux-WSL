9364f51392cb48509a22de99eba662545d8c8387
Xander <168613623+Xznder1984@users.noreply.github.com>
2026-03-08 11:57:10 +0700

    Update install.ps1

--- scripts/install.ps1
+++ scripts/install.ps1
@@ -1,10 +1,16 @@
 $DistroName = "VoidLinux"
-$InstallPath = "D:\WSL\VoidLinux"
-$RootFS = "rootfs\void-rootfs.tar.xz"
+$InstallDir = "D:\WSL\VoidLinux"
+$RootFSUrl = "https://github.com/Xznder1984/Void-Linux-WSL/releases/latest/download/void-rootfs.tar.xz"
 
 Write-Host "Installing Void Linux WSL..."
 
-wsl --import $DistroName $InstallPath $RootFS
+mkdir $InstallDir -ErrorAction Ignore
+
+Write-Host "Downloading rootfs..."
+Invoke-WebRequest $RootFSUrl -OutFile "$InstallDir\void-rootfs.tar.xz"
+
+Write-Host "Importing into WSL..."
+wsl --import $DistroName $InstallDir "$InstallDir\void-rootfs.tar.xz"
 
 Write-Host "Installation complete!"
 Write-Host "Run it with:"
